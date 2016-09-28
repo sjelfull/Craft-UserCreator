05905afa2b5ba5f6cbf66a2aa9f5eb2912e42372
Fred Carlsen <fred@sjelfull.no>
2016-09-28 09:54:43 +0200

    [Fixed] Fixed an issue where elevated session wasn't triggered when submitting the form

--- usercreator/UserCreatorPlugin.php
+++ usercreator/UserCreatorPlugin.php
@@ -59,7 +59,7 @@ class UserCreatorPlugin extends BasePlugin
      */
     public function getVersion ()
     {
-        return '1.0.2';
+        return '1.0.3';
     }
 
     /**
--- usercreator/resources/js/UserCreator_Script.js
+++ usercreator/resources/js/UserCreator_Script.js
@@ -7,39 +7,42 @@
  * @copyright Copyright (c) 2016 Fred Carlsen
  * @link      http://sjelfull.no
  * @package   UserCreator
- * @since     1.0.0
+ * @since     0.0.1
  */
 
 $(function () {
-    // If this Craft version supports elevated sessions
-    if (Craft.hasOwnProperty('ElevatedSessionForm')) {
+    var $form = $('#js-userCreatorForm');
+
+    if ($form.length && Craft.hasOwnProperty('ElevatedSessionForm')) {
         // Require an elevated session when submitting a form new
-        Craft.ElevatedSessionForm('#container');
+        new Craft.ElevatedSessionForm('#container');
     }
 
-    var userData = window.userCreatorCopyData;
-    var text = '';
+    if (window.hasOwnProperty('userCreatorCopyData')) {
+        var userData = window.userCreatorCopyData;
+        var text = '';
 
-    $.each(userData, function (index, user) {
-        text += '';
+        $.each(userData, function (index, user) {
+            text += '';
 
-        if (user.name !== '') {
-            text += Craft.t('Name') + ': ' + user.name + "\n";
-        }
+            if (user.name !== '') {
+                text += Craft.t('Name') + ': ' + user.name + "\n";
+            }
 
-        text += Craft.t('E-mail') + ': ' + user.email + "\n";
-        //text += 'Password: ' + user.password + "\n\n";
+            text += Craft.t('E-mail') + ': ' + user.email + "\n";
+            //text += 'Password: ' + user.password + "\n\n";
 
-        if (user.resetUrl) {
-            text += Craft.t('Set your password here') + ': ' + user.resetUrl + "\n\n";
-        }
-    });
+            if (user.resetUrl) {
+                text += Craft.t('Set your password here') + ': ' + user.resetUrl + "\n\n";
+            }
+        });
 
-    var clipboard = new Clipboard('.js-userCreatorCopyClipboard', {
-        text: function (trigger) {
-            return text;
-        }
-    });
+        var clipboard = new Clipboard('.js-userCreatorCopyClipboard', {
+            text: function (trigger) {
+                return text;
+            }
+        });
 
-    var clipboardResetUrls = new Clipboard('.js-copyResetUrl');
+        var clipboardResetUrls = new Clipboard('.js-copyResetUrl');
+    }
 });
